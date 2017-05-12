35046aef8e4be90e85e92e749d0767009a3c62a6
Zouron <jasrego@gmail.com>
2017-05-12 17:13:20 +1000

    Add annotation for birthday in person class

--- src/com/jasonrego/address/model/Person.java
+++ src/com/jasonrego/address/model/Person.java
@@ -2,6 +2,10 @@ package com.jasonrego.address.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import com.jasonrego.address.util.LocalDateAdapter;
+
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -106,7 +110,8 @@ public class Person {
     public StringProperty cityProperty() {
         return city;
     }
-
+    
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
     public LocalDate getBirthday() {
         return birthday.get();
     }
